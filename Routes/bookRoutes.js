75da9773b7d05caf9fbffa40f313b7810980602a
Rodolfo Estevam <rodolfoestevam.a@gmail.com>
2018-05-29 15:37:17 -0300

    refactoring code

--- Routes/bookRoutes.js
+++ Routes/bookRoutes.js
@@ -2,26 +2,12 @@ var express = require('express');
 
 var routes = function (Book) {
     var bookRouter = express.Router();
+    var bookController = require('../Controllers/bookController')(Book);
 
     bookRouter.route('/')
-        .post(function (req, res) {
-            var book = new Book(req.body);
-
-            book.save();
-            res.status(201).send(book);
-        })
-        .get(function (req, res) {
-            var query = {};
-            if (req.query.genre) {
-                query.genre = req.query.genre;
-            }
-            Book.find(query, function (err, books) {
-                if (err)
-                    res.status(500).send(err);
-                else
-                    res.json(books);
-            });
-        });
+        .post(bookController.post) 
+        .get(bookController.get);
+        
     bookRouter.use('/:bookId', function (req, res, next) {
         Book.findById(req.params.bookId, function (err, book) {
             if (err)
