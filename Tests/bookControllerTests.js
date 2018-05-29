c10af121edbe77ff5cc7a078c90530998b26b1b8
Rodolfo Estevam <rodolfoestevam.a@gmail.com>
2018-05-29 16:34:43 -0300

    adjustment on bookTests

--- Tests/bookControllerTests.js
+++ Tests/bookControllerTests.js
@@ -1,25 +1,30 @@
-var should = require ('should'),
-    sinon = require('sinon');
+var should = require('should'),
+    sinon = require('sinon'),
+    gulpMocha = require('gulp-mocha');
 
-describe('Book Controller Tests: ', function(){
-    it('shout not allow an empty on post', function(){
-        var Book = function(book){this.save = function(){}};
+describe('Book Controller Tests: ', function () {
+    describe('POST', function () {
+        it('shout not allow an empty on post', function () {
+            var Book = function (book) {
+                this.save = function () {}
+            };
 
-        var req ={
-            body: {
-                author: 'Jon Snow'
+            var req = {
+                body: {
+                    author: 'Jon Snow'
+                }
             }
-        }
-        var res = {
-            status: sinon.spy(),
-            send: sinon.spy(),
-        }
+            var res = {
+                status: sinon.spy(),
+                send: sinon.spy(),
+            }
 
-        var bookController = require('../Controllers/bookController')(Book);
+            var bookController = require('../Controllers/bookController')(Book);
 
-        bookController.post(req, res);
-        
-        res.status.calledWith(400).should.equal(true, 'Bad Status ' + res.status.args[0]);
-        res.send.calledWith('Title is required').should.equal(true);
+            bookController.post(req, res);
+
+            res.status.calledWith(400).should.equal(true, 'Bad Status ' + res.status.args[0]);
+            res.send.calledWith('Title is required').should.equal(true);
+        })
     })
 })
