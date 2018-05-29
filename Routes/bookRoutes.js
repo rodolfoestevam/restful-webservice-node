4d1f08212f4ed760c4a3e1fc40507783f51270fb
Rodolfo Estevam <rodolfoestevam.a@gmail.com>
2018-05-29 12:39:31 -0300

    Create routes folder to manage routes

---
+++ Routes/bookRoutes.js
@@ -0,0 +1,37 @@
+var express = require('express');
+
+var routes = function () {
+    var bookRouter = express.Router();
+
+    bookRouter.route('/Books')
+        .post(function (req, res) {
+            var book = new Book(req.body);
+            book.save();
+            res.status(201).send(book);
+        })
+        .get(function (req, res) {
+            var query = {};
+            if (req.query.genre) {
+                query.genre = req.query.genre;
+            }
+            Book.find(query, function (err, books) {
+                if (err)
+                    res.status(500).send(err);
+                else
+                    res.json(books);
+            });
+        });
+
+    bookRouter.route('/Books/:bookId')
+        .get(function (req, res) {
+            Book.findById(req.params.bookId, function (err, books) {
+                if (err)
+                    res.status(500).send(err);
+                else
+                    res.json(books);
+            });
+        });
+    return bookRouter;
+};
+
+module.exports = routes;
