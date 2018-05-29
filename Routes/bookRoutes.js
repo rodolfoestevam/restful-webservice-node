6edd81a1bcb6966f1fbc5bdc8ebcbb13220a40ac
Rodolfo Estevam <rodolfoestevam.a@gmail.com>
2018-05-29 15:05:32 -0300

    Implemented patch and delete

--- Routes/bookRoutes.js
+++ Routes/bookRoutes.js
@@ -22,27 +22,57 @@ var routes = function (Book) {
                     res.json(books);
             });
         });
-
+    bookRouter.use('/:bookId', function (req, res, next) {
+        Book.findById(req.params.bookId, function (err, book) {
+            if (err)
+                res.status(500).send(err);
+            else if (book) {
+                req.book = book;
+                next();
+            } else {
+                res.status(404).send('no book found');
+            }
+        });
+    });
     bookRouter.route('/:bookId')
         .get(function (req, res) {
-            Book.findById(req.params.bookId, function (err, book) {
-                if (err)
-                    res.status(500).send(err);
-                else
-                    res.json(books);
-            });
+            res.json(req.book);
         })
         .put(function (req, res) {
-            Book.findById(req.params.bookId, function (err, book) {
+            req.book.title = req.body.title;
+            req.book.author = req.body.author;
+            req.book.genre = req.body.genre;
+            req.book.read = req.body.read;
+            req.book.save(function (err) {
                 if (err)
                     res.status(500).send(err);
-                else
-                    book.title = req.body.title;
-                    book.author = req.body.author;
-                    book.genre = req.body.genre;
-                    book.read = req.body.read;
-                    book.save();
-                    res.json(book);
+                else {
+                    res.json(req.book);
+                }
+            });
+            res.json(req.book);
+        })
+        .patch(function (req, res) {
+            if (req.body._id)
+                delete req.body._id;
+            for (var p in req.body) {
+                req.book[p] = req.body[p];
+            }
+            req.book.save(function (err) {
+                if (err)
+                    res.status(500).send(err);
+                else {
+                    res.json(req.book);
+                }
+            });
+        })
+        .delete(function (req, res) {
+            req.book.remove(function (err) {
+                if (err)
+                    res.status(500).send(err);
+                else {
+                    res.status(500).send('Removed Sucessfully');
+                }
             });
         });
     return bookRouter;
