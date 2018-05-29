0ff2cb7d5e129e2b1bc8844ea60dc13a57e0006e
Rodolfo Estevam <rodolfoestevam.a@gmail.com>
2018-05-29 16:26:50 -0300

    bookController tests

---
+++ Tests/bookControllerTests.js
@@ -0,0 +1,25 @@
+var should = require ('should'),
+    sinon = require('sinon');
+
+describe('Book Controller Tests: ', function(){
+    it('shout not allow an empty on post', function(){
+        var Book = function(book){this.save = function(){}};
+
+        var req ={
+            body: {
+                author: 'Jon Snow'
+            }
+        }
+        var res = {
+            status: sinon.spy(),
+            send: sinon.spy(),
+        }
+
+        var bookController = require('../Controllers/bookController')(Book);
+
+        bookController.post(req, res);
+        
+        res.status.calledWith(400).should.equal(true, 'Bad Status ' + res.status.args[0]);
+        res.send.calledWith('Title is required').should.equal(true);
+    })
+})
