d69ca332fdff1abdea2af6696020d809e0ea3515
Rodolfo Estevam <rodolfoestevam.a@gmail.com>
2018-05-29 12:35:49 -0300

    POST + GET Working properly

--- app.js
+++ app.js
@@ -15,21 +15,32 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 
+bookRouter = require('./Routes/bookRoutes');
+
 var bookRouter = express.Router();
 
 bookRouter.route('/Books')
     .post(function (req, res) {
-        var book = new Book();
-        console.log(book);
-        res.send(book);
+        var book = new Book(req.body);
+        book.save();
+        res.status(201).send(book);
     })
     .get(function (req, res) {
         var query = {};
         if (req.query.genre) {
             query.genre = req.query.genre;
         }
-
         Book.find(query, function (err, books) {
+            if (err)
+                res.status(500).send(err);
+            else
+                res.json(books);
+        });
+    });
+
+bookRouter.route('/Books/:bookId')
+    .get(function (req, res) {
+        Book.findById(req.params.bookId, function (err, books) {
             if (err)
                 res.status(500).send(err);
             else
