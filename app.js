0b61498461084f99827557b126719adf16fa50b4
Rodolfo Estevam <rodolfoestevam.a@gmail.com>
2018-05-29 21:30:25 -0300

    adding ENV in app.js

--- app.js
+++ app.js
@@ -2,7 +2,13 @@ var express = require('express'),
     mongoose = require('mongoose'),
     bodyParser = require('body-parser');
 
-var db = mongoose.connect('mongodb://localhost/bookAPI');
+var db;
+console.log('Hello');
+if (process.env.ENV == 'Test')
+    db = mongoose.connect('mongodb://localhost/bookAPI_test');
+else {
+    db = mongoose.connect('mongodb://localhost/bookAPI');
+}
 
 var Book = require('./models/bookModel');
 
@@ -27,3 +33,5 @@ app.get('/', function (req, res) {
 app.listen(port, function () {
     console.log("Running on PORT:" + port);
 });
+
+module.exports = app;
